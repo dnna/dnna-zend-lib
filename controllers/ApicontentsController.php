22c6b509b058f751ee0ba9da3aa41030b302e78d
dnna@dnna.gr <dnna@dnna.gr@962bec3d-2152-6303-4b29-304c5289d42b>
2012-01-06 08:39:01 +0000

    ApiController now inherits from ApicontentsController

--- controllers/ApicontentsController.php
+++ controllers/ApicontentsController.php
@@ -62,5 +62,10 @@ class Dnna_Controller_ApicontentsController extends Zend_Rest_Controller
     public function schemaAction() {
         throw new Exception('No schema has been set for this resource');
     }
+
+    protected function utf8_urldecode($str) {
+        $str = preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode($str));
+        return html_entity_decode($str,null,'UTF-8');
+    }
 }
 ?>
